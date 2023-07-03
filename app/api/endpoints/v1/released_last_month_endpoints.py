1b4eb57cd1f65d66f02ab42ef5bbc30c5cc2b550
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-07-02 22:29:02 -0300

    fix: resolved bug of the response_model, change FastAPI for APIRouter in endpoints

--- app/api/endpoints/v1/released_last_month_endpoints.py
+++ app/api/endpoints/v1/released_last_month_endpoints.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.orm import Session
 
 import app.models
@@ -10,7 +10,7 @@ from database import conn
 
 conn.Base.metadata.create_all(bind=engine)
 
-router = FastAPI()
+router = APIRouter()
 
 
 # Dependency
@@ -21,7 +21,7 @@ def get_db():
     finally:
         db.close()
 
-@router.get("/users/", response_model=released_last_month_schemas.ReleasedGamesLastMonthSchema)
+@router.get("/users/", response_model=list[released_last_month_schemas.ReleasedGamesLastMonthSchema])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = released_last_month_controllers.get_users(db, skip=skip, limit=limit)
     return users
