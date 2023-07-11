60d767e092637f6d16d95eb1fab95282cca97433
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-07-10 21:45:21 -0300

    feat: create models for UpcomingGames

---
+++ app/models/upcoming_games_models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from database.conn import Base
+
+class UpcomingGamesModel(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    slug_game = Column(String, nullable=True)
+    name_game = Column(String, nullable=True)
+    playtime = Column(Integer, nullable=True)
+    plataform_id = Column(Integer, nullable=True)
+    plataform_name = Column(String, nullable=True)
+    plataform_slug = Column(String, nullable=True)
