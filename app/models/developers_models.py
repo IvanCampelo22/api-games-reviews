24658eef5f54ab55b25058330a3b6655eba4e6f2
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-07-11 08:26:29 -0300

    feat: create models for Developers

---
+++ app/models/developers_models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from database.conn import Base
+
+class Developers(Base):
+    __tablename__ = "developers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    developer_id = Column(Integer, nullable=True)
+    text = Column(String, nullable=True)
+    developer_name = Column(String, nullable=True)
+    exact_name = Column(String, nullable=True)
+    search_name = Column(String, nullable=True)
+    developer_slug = Column(String, nullable=True)
+    top_games = Column(String, nullable=True)
+    games_count = Column(Integer, nullable=True)
+    image_background = Column(String, nullable=True)
+    score = Column(String, nullable=True)
+
