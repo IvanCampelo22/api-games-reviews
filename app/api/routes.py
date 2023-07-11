beff30051e34c5185add286a14398c0ae86ed942
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-07-10 21:30:25 -0300

    fix: resolved bug in endpoint ReleasedLastMonthPlataform

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,5 +1,6 @@
 from fastapi.routing import APIRouter
-from app.api.endpoints.v1 import released_last_month_endpoints
+from app.api.endpoints.v1 import released_last_month_endpoints, released_last_month_plataform_endpoints
 api_router = APIRouter()
 
 api_router.include_router(released_last_month_endpoints.router, prefix='/games', tags=['released_last_month'])
+api_router.include_router(released_last_month_plataform_endpoints.router, prefix='/games', tags=['released_last_month_plataform'])
