8d5fec8f61efaac91832fb079ec6543a0998a859
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-07-18 07:11:33 -0300

    feat: consume api for DevelopersWithId

--- core/connection_api.py
+++ core/connection_api.py
@@ -44,3 +44,7 @@ class ApiGames:
     def get_developers(self, developer_name) -> None:
         response = self._make_request('GET', f'developers?search={developer_name}&page_size=1&key={self.api_key}')
         return response
+
+    def get_developers_with_id(self, start_date='', end_date='', developer_id='') -> None: 
+        response = self._make_request('GET', f'https://api.rawg.io/api/games?dates={start_date},{end_date}&developers={developer_id}&key={self.api_key}')
+        return response 
