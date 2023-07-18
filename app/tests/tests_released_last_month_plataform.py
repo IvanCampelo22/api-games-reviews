be30b4054fac9f818dd010b28b7b9cdcf068ed9b
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-07-18 12:00:13 -0300

    test: test for released_last_month

--- app/tests/tests_released_last_month_plataform.py
+++ app/tests/tests_released_last_month_plataform.py
@@ -9,7 +9,7 @@ client = TestClient(app)
 
 API_KEY = os.getenv("API_KEY_RAWG")
 
-def test_released_last_month():
+def test_released_last_month_plataform_endpoint():
 
     response = client.get(f"games/released-last-month-pataform/?start_date=2019-01-01&end_date=2019-01-30&plataform_id=7&key={API_KEY}")
 
