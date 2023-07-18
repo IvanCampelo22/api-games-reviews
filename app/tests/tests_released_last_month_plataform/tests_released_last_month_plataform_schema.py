01dd7ef9696d8b505862d84d6aa1de951f1bef1a
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-07-18 15:42:52 -0300

    test: create unit test for released_last_month_plataform check schema is invalid

--- app/tests/tests_released_last_month_plataform/tests_released_last_month_plataform_schema.py
+++ app/tests/tests_released_last_month_plataform/tests_released_last_month_plataform_schema.py
@@ -17,3 +17,16 @@ def test_valid_released_last_month_plataform():
 
     ReleasedGamesLastMonthSchemaMonthSchema(**data)
 
+
+def test_invalid_released_last_month_plataform():
+    invalid_data = {
+        "slug_game": "Diablo",
+        "name_game": "diablo",
+        "playtime": 10,
+        "plataform_id": 12,
+        "plataform_name": "PC",
+        "plataform_slug": "Pc"
+    }
+
+    with pytest.raises(ValidationError):
+        ReleasedGamesLastMonthSchemaMonthSchema(**invalid_data)
