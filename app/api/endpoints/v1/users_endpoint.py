65558ad88d9e7cc9595a5d36d34bdb28cb3dfc44
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-11-14 18:09:34 -0300

    feat: logout

--- app/api/endpoints/v1/users_endpoint.py
+++ app/api/endpoints/v1/users_endpoint.py
@@ -1,14 +1,15 @@
 from app.schemas.user_schema import UserCreate, TokenSchema, requestdetails, changepassword
 from app.models.users_models import User, TokenTable
-from database.conn import Base, engine, AnsyncSessionLocal
 from fastapi import FastAPI, Depends, HTTPException, status
+from app.auth.auth_bearer import JWTBearer
 from sqlalchemy.orm import Session
 from app.auth.auth_handler import get_hashed_password, create_access_token,create_refresh_token,verify_password
 from fastapi import FastAPI, Depends, HTTPException,status, APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import conn
 from sqlalchemy.future import select
-
+from jose import jwt
+from datetime import datetime
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30 
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 
@@ -62,7 +63,7 @@ async def login(request: requestdetails, session: AsyncSession = Depends(conn.ge
 from typing import List
 
 @router.get('/getusers', response_model=List[UserCreate])
-async def getusers(db: AsyncSession = Depends(conn.get_async_session)):
+async def getusers(dependencies=Depends(JWTBearer()), db: AsyncSession = Depends(conn.get_async_session)):
     async with db as session:
         query = select(User)
         result = await session.execute(query)
@@ -85,3 +86,42 @@ async def change_password(request: changepassword, session: AsyncSession = Depen
     session.commit()
     
     return {"message": "Password changed successfully"}
+
+@router.post('/logout')
+async def logout(dependencies=Depends(JWTBearer()), session: AsyncSession = Depends(conn.get_async_session)):
+    token = dependencies
+    payload = jwt.decode(token, JWT_SECRET_KEY, ALGORITHM)
+    user_id = payload['sub']
+    info = []
+    # Identificar user_ids a serem removidos
+    result = await session.execute(select(TokenTable))
+    token_record = result.scalars().all()
+
+    for record in token_record :
+        print("record",record)
+        if (datetime.utcnow() - record.created_date).days >1:
+            info.append(record.user_id)
+    
+    if info:
+        # Excluir tokens para os user_ids identificados
+        result = await session.execute(select(TokenTable).where(TokenTable.user_id.in_(info)))
+        existing_tokens = result.scalars().all()
+
+        for token in existing_tokens:
+            await session.delete()
+        
+    # Desativar o token atual
+    result = await session.execute(
+    select(TokenTable).where(
+        TokenTable.user_id == int(user_id),  # Converta user_id para inteiro
+        TokenTable.access_toke == str(token)  # Certifique-se de que token é uma string
+    )
+    )
+    existing_token = result.scalars().first()
+    
+    if existing_token:
+        existing_token.status = False
+        await session.commit()
+        await session.refresh(existing_token)
+
+    return {"message": "Logout Successfully"}
